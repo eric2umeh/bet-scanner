e97ee8ac9f6ad378bbf54bcb8a0cd7706f7581ff
Eric Umeohabike <32669797+eric2umeh@users.noreply.github.com>
2026-08-07 23:18:33 +0100

    Export tipster models from the models package.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.match import Match
 from app.models.odd import Odd
 from app.models.tip import Tip
+from app.models.tipster import BookingCode, Tipster
 
-__all__ = ["Match", "Odd", "Tip"]
+__all__ = ["Match", "Odd", "Tip", "Tipster", "BookingCode"]
